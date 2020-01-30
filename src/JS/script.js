855d86418777b361c3c0cb5762c195b8bd9d9a25
gravity-zero <gravity.neo@gmail.com>
2020-01-30 17:53:11 +0100

    start of the homepage with 2 unfix pb, reset.scss, image folder ...

---
+++ src/JS/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+//Function scrolling top from bottom
+
+function scrollLent()
+{
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    if(scrollTop > 0)
+    {
+        scrollBy(0, -5);
+        setTimeout(scrollLent, 5);
+    }
+    return true;
+}
