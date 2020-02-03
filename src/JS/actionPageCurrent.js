9e120360fa7199b879bf83a8b0846b6be6fdedd3
Hakim <cheboub.h@gmail.com>
2020-02-03 11:51:21 +0100

    js fuck

--- src/JS/actionPageCurrent.js
+++
@@ -1,8 +0,0 @@
-
-var path = document.location.href;
-var root  = path.substring( 0 ,path.lastIndexOf( "/" ) );
-var name     = path.substring(path.lastIndexOf( "/" )+1 );
-
-var recupName = name.split("#");
-var page = recupName[0];
-
