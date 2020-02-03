22c784de0cfbb80a2b84932e0792b19115b87d8e
j2frise <jarce2frise@yahoo.fr>
2020-02-03 10:58:23 +0100

    erge

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,18 +1,49 @@
+includeHeader()
+includeHTML();
+reloadPage();
 
-
-
-
-
-
-//Function scrolling top from bottom
-
-function scrollLent()
-{
-    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-    if(scrollTop > 0)
-    {
-        scrollBy(0, -5);
-        setTimeout(scrollLent, 5);
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("w3-include-html");
+      if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            /* Remove the attribute, and call this function once more: */
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /* Exit the function: */
+        return;
+      }
     }
-    return true;
-}
+  }
+
+  function includeHeader(){
+    var link = document.querySelector('link[rel="import"]');
+    
+        // Clone the <template> in the import.
+        var template = link.import.querySelector('template');
+        var clone = document.importNode(template.content, true);
+    
+        document.querySelector('#header').appendChild(clone);
+  }
+
+  function reloadPage() {
+    if(module.hot){
+        module.hot.accept(function(){
+            window.location.reload();
+        });
+    }
+  }
